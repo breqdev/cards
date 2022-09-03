9d89b2247fba7263d625b459a1d8dae010080fe0
Brooke Chalmers <breq@breq.dev>
2022-09-03 14:11:07 -0400

    breq16 -> breqdev

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,44 @@
-import React from "react"
-import { Page, Heading } from "@breq/react-theme"
-import { faGithub, faKeybase } from "@fortawesome/free-brands-svg-icons"
+import React from "react";
+import { Page, Heading } from "@breq/react-theme";
+import { faGithub, faKeybase } from "@fortawesome/free-brands-svg-icons";
 
-import TryIt from "./TryIt"
-import HelpText from "./HelpText"
+import TryIt from "./TryIt";
+import HelpText from "./HelpText";
 
 const links = {
-    github: "https://github.com/breq16/cards"
-}
+  github: "https://github.com/breqdev/cards",
+};
 
 const contact = [
-    {
-        text: "breq",
-        icon: faKeybase,
-        link: "https://keybase.io/breq"
-    },
-    {
-        text: "breq16",
-        icon: faGithub,
-        link: "https://github.com/breq16"
-    }
-]
+  {
+    text: "breq",
+    icon: faKeybase,
+    link: "https://keybase.io/breq",
+  },
+  {
+    text: "breqdev",
+    icon: faGithub,
+    link: "https://github.com/breqdev",
+  },
+];
 
 export default function App(props) {
-    return (
-        <Page
-            brand="cards by breq"
-            links={links}
-            contact={contact}
-            author="breq"
-            copyright="2021"
-            repo="Breq16/cards"
-        >
-            <Heading title="Cards" subtitle="Generate and embed digital cards featuring custom images and text." />
-            <TryIt />
-            <hr />
-            <HelpText />
-        </Page>
-    )
+  return (
+    <Page
+      brand="cards by breq"
+      links={links}
+      contact={contact}
+      author="breq"
+      copyright="2021"
+      repo="breqdev/cards"
+    >
+      <Heading
+        title="Cards"
+        subtitle="Generate and embed digital cards featuring custom images and text."
+      />
+      <TryIt />
+      <hr />
+      <HelpText />
+    </Page>
+  );
 }
